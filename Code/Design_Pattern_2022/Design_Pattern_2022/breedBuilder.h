0bf7859a410a7540adf4652a0e603087ef76fdf5
TanglinQAQ <3257138524@qq.com>
2022-12-04 20:13:10 +0800

    update

---
+++ Code/Design_Pattern_2022/Design_Pattern_2022/breedBuilder.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Data.h"
+
+using namespace DATA;
+
+    class breedBuilder {
+    public:
+        virtual void feed()=0;
+        virtual void graze()=0;
+        virtual void keephealthy()=0;
+
+        virtual Farming_Type getTyoe()=0;
+    };
+
+    class cattleBuilderImpl : public breedBuilder {
+    protected:
+        Farming_Type mycattle;
+    public:
+        void feed();
+        void graze();
+        void keephealthy();
+        Farming_Type getType() {
+            mycattle = Farming_Type::cattle;
+            return mycattle;
+        };
+    };
+
+    class chickenBuilderImpl : public breedBuilder {
+    protected:
+        Farming_Type mycattle;
+    public:
+        void feed();
+        void graze();
+        void keephealthy();
+        Farming_Type getType() {
+            mycattle = Farming_Type::chicken;
+            return mycattle;
+        };
+    };
+
+    class pigBuilderImpl : public breedBuilder {
+    protected:
+        Farming_Type mycattle;
+    public:
+        void feed();
+        void graze();
+        void keephealthy();
+        Farming_Type getType() {
+            mycattle = Farming_Type::pig;
+            return mycattle;
+        };
+    };
+
+
---
+++ Code/Design_Pattern_2022/Design_Pattern_2022/cattleBuilderImpl.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include"breedBuilder.h"
+
+using namespace std;
+
+void cattleBuilderImpl::feed() {
+    cout << "# 使用Builder模式：喂养牛" << endl;
+    cout << "喂养【牛】：喂食新鲜牧草" << endl;
+}
+
+void cattleBuilderImpl::graze() {
+    cout << "# 使用Builder模式：放牧牛" << endl;
+    cout << "放牧【牛】：河边放养" << endl;
+}
+
+void cattleBuilderImpl::keephealthy() {
+    cout << "# 使用Builder模式：保持牛健康" << endl;
+    cout << "保持健康【牛】：接种牛疫苗" << endl;
+}
