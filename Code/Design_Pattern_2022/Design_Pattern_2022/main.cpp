263e66c2e98fe3b76637a7a839787b2500f2a75f
Zzl3 <91377542+Zzl3@users.noreply.github.com>
2022-12-05 11:59:27 +0800

    Revert "迭代器模式"
    This reverts commit 09aec37282ef76923ed5a60218ba165756bd917c, reversing
    changes made to c556742c997d47590fca9871be917556d311f30e.

--- Code/Design_Pattern_2022/Design_Pattern_2022/main.cpp
+++ Code/Design_Pattern_2022/Design_Pattern_2022/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*主函数，用于调用各功能的测试模块*/
 #include <iostream>
 #include "Data.h"
@@ -38,41 +37,4 @@ int main()
 			break;
 		}
 	}
-=======
-/*主函数，用于调用各功能的测试模块*/
-#include <iostream>
-#include "Data.h"
-#include "test.h"
-using namespace std;
-using namespace DATA;
-
-int main()
-{
-	Database DB;
-	while (1) {
-		cout << "请选择现在要进行的操作：" << endl;
-		int oper;
-		cin >> oper;
-		switch (oper) {
-		case 1:
-			//测试采购
-			FarmStocking_Client();
-			break;
-		case 2:
-			//测试生产流程
-			State_manage();
-			break;
-		case 3:
-			//测试访问工厂
-			Visitor_manage();
-			break;
-		case 9:
-			//测试桥接模式
-			Bridge_Manage();
-			break;
-		default:
-			break;
-		}
-	}
->>>>>>> 8ea6aac2dcaba61211688306be04ca5a9efa49dd
 }
--- Code/Design_Pattern_2022/Design_Pattern_2022/test.h
+++ Code/Design_Pattern_2022/Design_Pattern_2022/test.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #include "Data.h"
 using namespace DATA;
@@ -7,10 +6,3 @@ void AdapterProcessing_Client();
 void State_manage();
 void Visitor_manage();
 void IteratorProcessing_Client(Database& DB);
-=======
-#pragma once
-void FarmStocking_Client();
-void State_manage();
-void Visitor_manage();
-void Bridge_Manage();
->>>>>>> 8ea6aac2dcaba61211688306be04ca5a9efa49dd
