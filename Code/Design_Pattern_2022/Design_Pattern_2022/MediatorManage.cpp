98ea47a8ff88c76706e59ccd8d15fd8c88070614
Steven.Liu <xueyi656@gmail.com>
2022-12-11 03:24:51 +0800

    完成中介者模式

---
+++ Code/Design_Pattern_2022/Design_Pattern_2022/MediatorManage.cpp
@@ -0,0 +1,14 @@
+#include "Person.h"
+
+void MediatorManage()
+{
+    TradeMediator* mediator = new TradeMediator();
+    Wholesaler* host = new Wholesaler();
+    host->setMediator(mediator);
+    Consumer* client = new Consumer();
+    client->setMediator(mediator);
+    mediator->setClient(client);
+    mediator->setHost(host);
+    client->sendMessage("我要买鸡100只");
+    host->sendMessage("这里有鸡300只要出售");
+}
---
+++ Code/Design_Pattern_2022/Design_Pattern_2022/Person.h
@@ -0,0 +1,54 @@
+#pragma once
+//我们用中介者模式来模拟并管理批发商和消费者之间的关系
+#include<iostream>
+using namespace std;
+
+class Mediator {
+public:
+	virtual void setHost(Person* p) = 0;
+	virtual void setClient(Person* p) = 0;
+	virtual void sendMsg(Person* p, string msg) {};
+};
+
+class Person {
+public:
+	virtual void setMediator(Mediator* mediator) {};
+	virtual void sendMessage(string msg) = 0;
+	virtual void getMessage() {};
+protected:
+	Mediator* m_mediator;
+};
+
+class TradeMediator :public Mediator
+{
+public:
+    void setHost(Person* p);
+
+    void setClient(Person* p);
+
+	void sendMsg(Person* p, string msg);
+
+private:
+    Person* m_host;
+    Person* m_client;
+};
+
+class Wholesaler :public Person
+{
+public:
+    void setMediator(Mediator* mediator);
+
+    void sendMessage(string msg);
+
+	void getMessage(string msg);
+};
+
+class Consumer :public Person
+{
+public:
+    void setMediator(Mediator* mediator);
+
+    void sendMessage(string msg);
+
+	void getMessage(string msg);
+};
